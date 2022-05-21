669619a77c0a660fea6b72def3d973203cbe5cc1
proalamin-247 <pro.alamin247@gmail.com>
2022-05-21 13:10:05 +0600

    show logout btn and logout added

--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -1,28 +1,30 @@
 import React from 'react';
-import {  useCreateUserWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import {  useCreateUserWithEmailAndPassword, useSignInWithGoogle, useUpdateProfile } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
 import Loading from '../Shared/Loading';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 
 const SignUp = () => {
     const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
     const { register, formState: { errors }, handleSubmit } = useForm();
-    
+    const navigate = useNavigate();
+
     const [
         createUserWithEmailAndPassword,
         user,
         loading,
         error,
     ] = useCreateUserWithEmailAndPassword(auth);
+    const [updateProfile, updating, updateError] = useUpdateProfile(auth);
 
     let signINErrorMessage;
 
-    if (loading || gLoading) {
+    if (loading || gLoading || updating) {
         return <Loading></Loading>
     }
-    if (error || gError) {
+    if (error || gError || updateError) {
         signINErrorMessage = <p className='text-red-500'><small>{error?.message || gError?.message}</small></p>
     }
 
@@ -31,9 +33,12 @@ const SignUp = () => {
         console.log(user || gUser);
     }
 
-    const onSubmit = data => {
-        console.log(data);
-        createUserWithEmailAndPassword(data.email, data.password);
+    const onSubmit = async data => {
+        await createUserWithEmailAndPassword(data.email, data.password);
+        await updateProfile({ displayName: data.name });
+        console.log('update name');
+        navigate('/appointment');
+
     };
     return (
         <div className='flex justify-center items-center h-screen'>
